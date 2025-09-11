b02fc20e9013dfeece606971a43c4b0c4a5c8246
Saud Waqar <saudwaqarmalik@gmail.com>
2025-09-11 08:02:25 +0500

    ABC-123 Setting up Project

---
+++ GraphQL-LLM/GraphQL-LLM/GraphQL_LLMApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphQL_LLMApp.swift
+//  GraphQL-LLM
+//
+//  Created by Saud Waqar on 11/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct GraphQL_LLMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
